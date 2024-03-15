ee0e3fc8904999305c4f26efa6e2b283cc8cc86b
Emil324234 <145430552+Emil324234@users.noreply.github.com>
2024-03-16 00:01:16 +0300

    Add files via upload

---
+++ MainWindow(DataSet).xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using prct1lab.DS3DataSetTableAdapters;
+
+namespace prct1lab
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        WeaponsTableAdapter weapons = new WeaponsTableAdapter();
+        public MainWindow()
+        {
+            InitializeComponent();
+            WeaponsComboBox.ItemsSource = weapons.GetData();
+            WeaponsComboBox.DisplayMemberPath = "WeaponName";
+        }
+        private void WeaponComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            object cellID = (WeaponsComboBox.SelectedItem as DataRowView).Row[0];
+            object WeaponName = (WeaponsComboBox.SelectedItem as DataRowView).Row[1];
+
+            MessageBox.Show($"ID: {cellID}, Weapon Name: {WeaponName}");
+        }
+    }
+}
---
+++ MainWindowEF.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace prct1labEF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DS3Entities context = new DS3Entities();
+        public MainWindow()
+        {
+            InitializeComponent();
+            WeaponGRD.ItemsSource = context.Weapons.ToList();
+        }
+        private void WeaponDRG_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selected = WeaponGRD.SelectedItem as Weapons;
+        }
+        private void WeaponDRG_Selection(object sender, SelectionChangedEventArgs e)
+        {
+            var selected = WeaponGRD.SelectedItem as Weapons;
+            MessageBox.Show(selected.Weapon_ID.ToString()); 
+            MessageBox.Show(selected.WeaponClass);
+        }
+    }
+}
